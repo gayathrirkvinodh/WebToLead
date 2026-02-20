84dd1dd7f771b4280cc847885a2852010a92af96
Gayathri <radhakrishnangayathri8@gmail.com>
2026-02-20 14:45:53 -0500

    webToLeadForm  Commit 1

---
+++ script.js
@@ -0,0 +1,10 @@
+function handleSubmit(event){
+    let inpElt = document.querySelector('.LeadDateInput');
+    if(inpElt){
+        let opElt = document.querySelector('.LeadDateOutput');
+        if(opElt){
+            let leadDate = new Date(inpElt.value).toLocaleDateString('en-US');
+            opElt.value = leadDate;
+        }
+    }
+}
