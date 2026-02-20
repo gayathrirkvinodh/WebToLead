e4270618f8fd18f9c33db69d3889ad01edcdfc05
Gayathri <radhakrishnangayathri8@gmail.com>
2026-02-20 17:41:26 -0500

    Google Captcha

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+let updated = false;
+
 function handleSubmit(event){
-    let inpElt = document.querySelector('.LeadDateInput');
-    if(inpElt){
-        let opElt = document.querySelector('.LeadDateOutput');
-        if(opElt){
-            let leadDate = new Date(inpElt.value).toLocaleDateString('en-US');
-            opElt.value = leadDate;
-            console.log('Test modified');
+    if(updated){
+         let inpElt = document.querySelector('.LeadDateInput');
+        if(inpElt){
+            let opElt = document.querySelector('.LeadDateOutput');
+            if(opElt){
+                let leadDate = new Date(inpElt.value).toLocaleDateString('en-US');
+                opElt.value = leadDate;
+                console.log('Test modified');
+            }
         }
     }
+    else{
+        alert('Please update the captcha!!');
+        event.preventDefault();
+    }
+   
 }
+
+ function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+ function updateCaptcha(){
+    updated = true;
+ }
